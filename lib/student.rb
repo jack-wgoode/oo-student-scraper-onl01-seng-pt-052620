1daa3d8882b0342da0fb99ed738611c245f78902
Jack Goode <jack.w.goode@icloud.com>
2020-07-18 15:10:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
 
   @@all = []
 
-  def initialize(student_hash)
+  def new(student_hash)
      attributes.each do |attribute_name, attribute_value|
           if self.respond_to?("#{attribute_name}=") #is it there?
               self.send("#{attribute_name}=", attribute_value)
