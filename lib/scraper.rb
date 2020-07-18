6f6c27b8fe8b2c7d378fbf56a98837239580a886
Jack Goode <jack.w.goode@icloud.com>
2020-07-18 14:24:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,9 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open("https://learning.flatironschool.com/courses/154/pages/oo-student-scraper")
+    Nokogiri::HTML(html)
+    doc = Nokogiri::HTML(html)
     
   end
 
