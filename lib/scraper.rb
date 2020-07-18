df3e978dd5df383e21ac6af5ccf201c4e1ce57dc
Jack Goode <jack.w.goode@icloud.com>
2020-07-18 15:07:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -39,7 +39,7 @@ class Scraper
           return_hash[:blog] = element.attr('href')
         end
       
-      return_hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
+      #return_hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
       return_hash[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
     end
   return_hash
